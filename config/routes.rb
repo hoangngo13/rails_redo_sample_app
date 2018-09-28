ef96426703830f03f96432c49cf862a6a12c1b16
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@framgia.com>
2018-09-28 11:45:29 +0700

    Finish static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user/new'
+  get 'user/index'
+  get 'user/edit'
+  get 'user/show'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/help'
+  get 'static_pages/contact'
+  resources :user
 end
