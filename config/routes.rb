2b661e3ab509496f3671e891eb44e4948ae4f139
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@framgia.com>
2018-09-28 15:45:22 +0700

    Finish user basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get 'user/index'
-  get 'user/edit'
-  get 'user/show'
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/help'
   get 'static_pages/contact'
-  resources :user
+  resources :users
 end
