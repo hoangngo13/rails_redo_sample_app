249274ce475445e733286a9c2244a8a43917ed1f
Ngo Dinh Nhat Hoang <ngo.dinh.nhat.hoang@framgia.com>
2018-10-01 13:50:47 +0700

    Finish advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       flash[:infor] = "Login sucess!"
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = "Email or password is not valid"
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
